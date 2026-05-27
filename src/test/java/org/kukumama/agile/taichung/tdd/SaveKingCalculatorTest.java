075ce271d14ecb835ee9aa4aa2e0e5e72651628b
Yu-Song Syu <kuma@cloud-latitude.com>
2026-05-27 22:51:22 +0800

    beginning of Agile Taichung 2026/05/29

--- src/test/java/org/kukumama/agile/taichung/tdd/SaveKingCalculatorTest.java
+++ src/test/java/org/kukumama/agile/taichung/tdd/SaveKingCalculatorTest.java
@@ -1,12 +1,18 @@
 package org.kukumama.agile.taichung.tdd;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class SaveKingCalculatorTest {
 
     @Test
     void one_candidate() {
-        
+
+        SaveKingCalculator sut = new SaveKingCalculator();
+
+        String actual = sut.calculate("Chen");
+
+        Assertions.assertThat(actual).isEqualTo("Chen");
 
     }
 }
